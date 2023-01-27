a4173b0dfffec3cb394ffe8a95b42eb5d24152d5
Quark <gennadiy.kaydalov@mail.ru>
2023-01-27 04:45:00 +0200

    Added separated mesh function
    Still needed performance improvement

--- Engine/Collision.h
+++ Engine/Collision.h
@@ -9,6 +9,7 @@ private:
 
 private:
 	friend class Collision;
+	friend class Graph;
 };
 
 class Collision : public Module {
@@ -26,4 +27,19 @@ private:
 	void SearchFrom(unsigned int from, std::vector<int>* vector);
 	void SwapToEndAndDelete(unsigned int from, std::vector<int> *vector);
 	void InsertTriangle(unsigned int PrimitiveObjectID, unsigned int TriangleID, unsigned int TriangleNum ,std::vector<int> *AdjacentTriangles);
+};
+
+class Graph {
+private:
+	int numVertices;
+
+	std::vector<std::vector<int>> adjLists;
+	std::vector<bool> visited;
+
+public:
+	void AddEdge(int Src, int Dest);
+	void DFS(int StartVertex, std::vector<int>* Output);
+	unsigned int GetAdjListSize();
+
+	void BFS(int StartVertex, std::vector<int>* Output);
 };
