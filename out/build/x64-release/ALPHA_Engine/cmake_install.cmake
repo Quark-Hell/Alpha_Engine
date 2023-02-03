89299c9707e84c46e87b46e0a70e409d6de2bbd7
Quark <gennadiy.kaydalov@mail.ru>
2023-02-03 03:51:06 +0200

    Added copying resources to build folder

--- out/build/x64-release/ALPHA_Engine/cmake_install.cmake
+++ out/build/x64-release/ALPHA_Engine/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/genna/source/repos/3DRotate/ALPHA_Engine
+# Install script for directory: C:/Users/genna/source/repos/3D-Engine/ALPHA_Engine
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/genna/source/repos/3DRotate/out/install/x64-release")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/genna/source/repos/3D-Engine/out/install/x64-release")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
--- out/build/x64-release/cmake_install.cmake
+++ out/build/x64-release/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/genna/source/repos/3DRotate
+# Install script for directory: C:/Users/genna/source/repos/3D-Engine
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Users/genna/source/repos/3DRotate/out/install/x64-release")
+  set(CMAKE_INSTALL_PREFIX "C:/Users/genna/source/repos/3D-Engine/out/install/x64-release")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -34,7 +34,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("C:/Users/genna/source/repos/3DRotate/out/build/x64-release/ALPHA_Engine/cmake_install.cmake")
+  include("C:/Users/genna/source/repos/3D-Engine/out/build/x64-release/ALPHA_Engine/cmake_install.cmake")
 
 endif()
 
@@ -46,5 +46,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/genna/source/repos/3DRotate/out/build/x64-release/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/genna/source/repos/3D-Engine/out/build/x64-release/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
