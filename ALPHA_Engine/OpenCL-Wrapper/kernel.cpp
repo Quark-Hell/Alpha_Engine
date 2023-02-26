8f3abc15339878c37f073c614d1216220062b817
Quark <gennadiy.kaydalov@mail.ru>
2023-02-26 23:00:17 +0200

    Added OpenCL-Wrapper

---
+++ ALPHA_Engine/OpenCL-Wrapper/kernel.cpp
@@ -0,0 +1,13 @@
+#include "kernel.hpp" // note: unbalanced round brackets () are not allowed and string literals can't be arbitrarily long, so periodically interrupt with )+R(
+string opencl_c_container() { return R( // ########################## begin of OpenCL C code ####################################################################
+
+
+
+kernel void add_kernel(global float* A, global float* B, global float* C) { // equivalent to "for(uint n=0u; n<N; n++) {", but executed in parallel
+	const uint n = get_global_id(0);
+	C[n] = A[n]+B[n];
+}
+
+
+
+);} // ############################################################### end of OpenCL C code #####################################################################
