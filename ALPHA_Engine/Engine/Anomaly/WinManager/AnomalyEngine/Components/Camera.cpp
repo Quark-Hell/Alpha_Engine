dce62ba6802959fbdab0f6e1fe520bb1d7d8c16f
Quark <gennadiy.kaydalov@mail.ru>
2024-10-13 08:42:49 +0300

    Added camera and multi-windows render

---
+++ ALPHA_Engine/Engine/Anomaly/WinManager/AnomalyEngine/Components/Camera.cpp
@@ -0,0 +1,5 @@
+//
+// Created by admin on 13.10.24.
+//
+
+#include "Camera.h"
---
+++ ALPHA_Engine/Engine/Anomaly/WinManager/AnomalyEngine/Components/Camera.h
@@ -0,0 +1,16 @@
+//
+// Created by admin on 13.10.24.
+//
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+
+
+class Camera {
+
+};
+
+
+
+#endif //CAMERA_H
