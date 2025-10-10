57ba43629c44808c86f4d4f22090997af4dc20e0
Quark <gennadiy.kaydalov@mail.ru>
2025-10-10 03:14:52 +0300

    review

--- ALPHA_Engine/Engine/AnomalyEngine/Components/BaseLight.h
+++ ALPHA_Engine/Engine/AnomalyEngine/Components/BaseLight.h
@@ -5,12 +5,12 @@
 
 namespace AnomalyEngine {
     class BaseLight : public Core::Component {
-        public:
-            glm::vec3 Color{1,1,1};
-            float Intensity = 1.0f;
+    public:
+        glm::vec3 Color{ 1,1,1 };
+        float Intensity = 1.0f;
 
-        protected:
-            BaseLight() = default;
-            ~BaseLight() override = default;
+    protected:
+        BaseLight() = default;
+        ~BaseLight() override = default;
     };
 }
